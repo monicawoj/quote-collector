703b764f7bb637c521da6e424244103240d343bd
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-07-31 21:34:56 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Quote.create(quote: 'And now these three remain: faith, hope and love. But the greatest of these is love.', author: '1 Corinthians 13:13', link: '', category: 'Religion')
+Quote.create(quote: 'Choose a job you love, and you will never have to work a day in your life', author: 'Confucius', link: '', category: 'Career')
+Quote.create(quote: 'If you don't like it, change it. If you can't change it, learn to like it.', author: 'Wujek Adam', link: '', category: 'Everyday')
