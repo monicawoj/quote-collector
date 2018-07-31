b0d647b709c7fcbe2db21bfae2aee97f38b6b125
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-07-31 21:38:23 +0200

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,4 @@
 #
 Quote.create(quote: 'And now these three remain: faith, hope and love. But the greatest of these is love.', author: '1 Corinthians 13:13', link: '', category: 'Religion')
 Quote.create(quote: 'Choose a job you love, and you will never have to work a day in your life', author: 'Confucius', link: '', category: 'Career')
-Quote.create(quote: 'If you don't like it, change it. If you can't change it, learn to like it.', author: 'Wujek Adam', link: '', category: 'Everyday')
+Quote.create(quote: 'If you do not like it, change it. If you cannot change it, learn to like it.', author: 'Wujek Adam', link: '', category: 'Everyday')
