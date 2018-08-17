47ce628c5febd5afd2cc9877d7ef762a1d7f4a0f
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-08-17 21:11:45 +0200

    removed max characters for quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ApplicationRecord
-	validates :quote, presence: true, length: { minimum: 10, maximum: 180 }
+	validates :quote, presence: true, length: { minimum: 10 }
 	validates :category, presence: true
 end
